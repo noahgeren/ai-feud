ccc4a9ed7fa18f53c7aafdbfb0ec740d96166774
Noah Geren <40411652+noahgeren@users.noreply.github.com>
2022-10-23 15:56:24 -0500

    Initial commit

---
+++ src/router.ts
@@ -0,0 +1,11 @@
+import { createRouter, createWebHistory} from "vue-router";
+import Index from "./pages/Index.vue";
+
+const routes = [
+    { path: "/", component: Index } 
+];
+
+export const router = createRouter({
+    history: createWebHistory(),
+    routes
+});
