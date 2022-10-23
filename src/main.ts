e67297f5522f30acce35e5ce516ef9f86459af94
Noah Geren <40411652+noahgeren@users.noreply.github.com>
2022-10-23 16:27:53 -0500

    Added firebase

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,17 @@
-import { createApp } from "vue";
-import "./style.css";
 import App from "./App.vue";
+import { createApp } from "vue";
 import { router } from "./router";
+import { initializeApp } from "firebase/app";
+import "./style.css";
+
+initializeApp({
+    apiKey: "AIzaSyCsHnYxZdZ0qfDZrifoOPiH-Ps0soYlDuU",
+    authDomain: "aifeud.firebaseapp.com",
+    databaseURL: "https://aifeud-default-rtdb.firebaseio.com",
+    projectId: "aifeud",
+    storageBucket: "aifeud.appspot.com",
+    messagingSenderId: "777427713264",
+    appId: "1:777427713264:web:b61a4577ef4c12f1901635"
+});
 
 createApp(App).use(router).mount("#app");
