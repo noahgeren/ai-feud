96da50854280ab2942ad11e070245c13ab652371
Noah Geren <40411652+noahgeren@users.noreply.github.com>
2022-10-23 17:24:06 -0500

    Added daisyUI and index page design

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -3,5 +3,17 @@
 </script>
 
 <template>
-    <h1>Hello World!</h1>
+    <div class="hero min-h-screen bg-base-200">
+        <div class="hero-content text-center">
+            <div class="max-w-lg">
+                <h1 class="text-8xl font-bold">AI Feud</h1>
+                <p class="py-5">A Family Feud inspired game that uses GPT-3 to generate answers.</p>
+                <button class="btn btn-primary mx-3">Start Game</button>
+                <button class="btn btn-secondary mx-3">Join Game</button>
+            </div>
+        </div>
+        <footer class="absolute bottom-3 right-4">
+            Created by <a href="https://github.com/noahgeren" target="_blank" class="link link-primary">Noah Geren</a>
+        </footer>
+    </div>
 </template>
