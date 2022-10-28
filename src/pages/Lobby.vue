6111789a3d99d111a224154f35a91740dbeb4b73
Noah Geren <40411652+noahgeren@users.noreply.github.com>
2022-10-28 18:01:23 -0500

    Lobby design changes

--- src/pages/Lobby.vue
+++ src/pages/Lobby.vue
@@ -13,23 +13,23 @@ const ready = computed(() => Object.keys(game.value.players || {}).length >= 2);
         <h3 class="text-5xl underline">Access Code</h3>
         <h2 class="text-9xl font-bold">{{ game.id }}</h2>
         <h4 class="text-2xl">Join at AIFeud.web.app</h4>
-        <h3 class="text-5xl underline mt-10">Teams</h3>
+        <h3 class="text-5xl mt-10">Teams</h3>
     </div>
-    <div class="flex flex-grow max-w-3xl w-full self-center mt-5 mb-3">
-        <div class="w-1/2 px-5 border-r">
+    <div class="flex flex-grow max-w-3xl w-full self-center mt-5 mb-3 text-center">
+        <div class="w-1/2 px-5">
             <input v-model="teamOneName" type="text" class="input input-lg input-bordered shadow-lg w-full" />
             <ul>
-                <li class="text-xl py-2 px-6">Player 1</li>
-                <li class="text-xl py-2 px-6">Player 2</li>
-                <li class="text-xl py-2 px-6">Player 3</li>
+                <li class="text-xl py-2">Player 1</li>
+                <li class="text-xl py-2">Player 2</li>
+                <li class="text-xl py-2">Player 3 dsafsdfa</li>
             </ul>
         </div>
-        <div class="w-1/2 px-5 border-l">
+        <div class="w-1/2 px-5">
             <input v-model="teamTwoName" type="text" class="input input-lg input-bordered shadow-lg w-full" />
             <ul>
-                <li class="text-xl py-2 px-6">Player 1</li>
-                <li class="text-xl py-2 px-6">Player 2</li>
-                <li class="text-xl py-2 px-6">Player 3</li>
+                <li class="text-xl py-2">Player 1</li>
+                <li class="text-xl py-2">Player 2</li>
+                <li class="text-xl py-2">Player 3</li>
             </ul>
         </div>
     </div>
